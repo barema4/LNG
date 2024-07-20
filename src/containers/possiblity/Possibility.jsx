e8e9585b2cb31468eb4097182310915ce0589846
barema4 <samrubarema6@gmail.com>
2024-07-20 18:39:10 +0300

    remove un used data

--- src/containers/possiblity/Possibility.jsx
+++ src/containers/possiblity/Possibility.jsx
@@ -1,17 +1,5 @@
 import React from "react";
-import { Carousel, Col, Row } from "antd";
-import Slack from "../../assets/slack.png";
-import Shop from "../../assets/shopify.png";
-
-const contentStyle = {
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-  width: "500px",
-  margin: 0,
-};
+import {Col, Row } from "antd";
 
 const Possibility = () => (
   <Row style={{ margin: "1rem 6rem", display: 'flex', flexDirection: 'column' }}>
